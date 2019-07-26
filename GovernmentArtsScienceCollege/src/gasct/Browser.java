7d44adeb74e3ff78c55723e00ddb4c0c4ada02f4
thru <thirum1304@gmail.com>
2019-07-26 18:11:01 +0530

    Change-Id: I12c1ccb14be6eabea81bf7b0edc95a8eeddceb64 Signed-off-by: thru <thirum1304@gmail.com>

--- GovernmentArtsScienceCollege/src/gasct/Browser.java
+++ GovernmentArtsScienceCollege/src/gasct/Browser.java
@@ -1,8 +1,6 @@
 package gasct;
 
 
-import java.io.IOException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -12,12 +10,12 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Browser {
 
-	static WebDriver driver=null;
+	static WebDriver driver;
 	
 public static WebElement username;
 public static WebElement password;
@@ -27,19 +25,20 @@ public static WebElement captcha;
 @FindBy(xpath="/html/body/div/div[2]/form/div[5]/div[1]/button")
 public static WebElement submit;
 
-@BeforeTest
+@BeforeSuite
 public static void beforelogin(){
 	
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\GatedONTech\\Downloads\\Imp\\chromedriver.exe");
 	
-	WebDriver driver=new ChromeDriver();
-	
+	driver=new ChromeDriver();
+	driver.get("http://gatedontechnologies.com/college/gtadmin/login/");
 	
 }
 
 @Test
 	public static void homepage() {
-	driver.get("http://gatedontechnologies.com/college/gtadmin/login/");
+	//beforelogin();
+	
 		PageFactory.initElements(driver, Browser.class);
 		
 		//driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -49,10 +48,10 @@ public static void beforelogin(){
 		//WebElement password = driver.findElement(By.name("password"));
 		password.sendKeys("12345");
 		
-		WebDriverWait wait = new WebDriverWait(driver, 20);
+		WebDriverWait wait = new WebDriverWait(driver, 10);
 		
 		
-		wait.until(ExpectedConditions.textToBePresentInElementValue(By.xpath("/html/body/div/div[2]/form/div[4]/input"),"?"));
+		wait.until(ExpectedConditions.textToBe(By.xpath("html/body/div/div[2]/form/div[4]/input"), "?"));
 		
 		driver.findElement(By.xpath("/html/body/div/div[2]/form/div[5]/div[1]/button")).sendKeys(Keys.ENTER);
 		//File images = image.getScreenshotAs(OutputType.FILE);
